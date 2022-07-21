5cde8e016ba57ccb46f52009a87b92af85d022ec
Yasmine <yasmine.hara.castro@gmail.com>
2022-07-21 15:27:33 -0700

    started backend and erros

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,9 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   staging: {
